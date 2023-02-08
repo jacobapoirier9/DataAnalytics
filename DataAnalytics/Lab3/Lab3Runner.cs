9cd6e2bbef8e7639762f36c9c223ac3cef9db660
Jacob Poirier <jacobapoirier9@gmail.com>
2023-02-08 15:32:18 -0500

    create Customer class

--- DataAnalytics/Lab3/Lab3Runner.cs
+++ DataAnalytics/Lab3/Lab3Runner.cs
@@ -7,3 +7,29 @@ public static class Lab3Runner
 
     }
 }
+
+public class Customer
+{
+    private readonly string _firstName;
+    private readonly string _lastName;
+    private readonly string _address;
+    private readonly string _city;
+    private readonly string _state;
+    private readonly string _zipCode;
+    private readonly string _phoneNumber;
+    private readonly string _email;
+    private readonly string _photoLink;
+
+    public Customer(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string email, string photoLink)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        _address = address;
+        _city = city;
+        _state = state;
+        _zipCode = zipCode;
+        _phoneNumber = phoneNumber;
+        _email = email;
+        _photoLink = photoLink;
+    }
+}
