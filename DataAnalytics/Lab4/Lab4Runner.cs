398a8a7bfbff7f94cd81205a2f0184cc4779814f
Jacob Poirier <jacobapoirier9@gmail.com>
2023-02-13 16:36:09 -0500

    create RunWithListLooping

--- DataAnalytics/Lab4/Lab4Runner.cs
+++ DataAnalytics/Lab4/Lab4Runner.cs
@@ -6,8 +6,53 @@ public static class Lab4Runner
 {
     private const string _rootsite = "http://citelms.net/Internships/Summer_2018/Fan_Site/";
 
-    public static void Run()
+    public static void RunWithListLooping()
     {
+        var dictionary = new Dictionary<string, string>();
+
+        var unvisitedHrefs = new List<string>() { _rootsite + "index.html" };
+        var visitedHrefs = new List<string>();
+
+        while (unvisitedHrefs.Count > 0)
+        {
+            var currentUrl = unvisitedHrefs[0];
+            if (visitedHrefs.Contains(currentUrl))
+            {
+                unvisitedHrefs.RemoveAt(0);
+                continue;
+            }
+
+            var html = GetRootPageHtml(currentUrl);
+
+            var title = Regex.Match(html, @"<title>(?<Title>.+)</title>").Groups["Title"].Value;
+            dictionary.Add(title, currentUrl);
+
+            var pageUrls = Regex.Matches(html, @"<a\s.*href=['""](?<Url>[^'""<>:#]*)['""]>.*<\/a>") // Regex will not return an href that contains the ':' character (filter out http links)
+                    .Select(match => _rootsite + match.Groups["Url"].Value)
+                    .Where(href =>
+                    {
+                        if (href.EndsWith(".pdf") || href.EndsWith(".png")) // Filter out PDF and PNG documents
+                            return false;
+
+                        else if (href.Contains('#')) // Filter out links where the URL contains an ID to auto scroll to - Page has already been downloaded.
+                            return false;
+
+                        else if (visitedHrefs.Contains(href)) // Filter out links already visited
+                            return false;
+
+                        else
+                            return true;
+                    }).ToList();
+
+            unvisitedHrefs.AddRange(pageUrls);
+
+            visitedHrefs.Add(currentUrl);
+        }
+
+        foreach (var record in dictionary)
+        {
+            Console.WriteLine($"{record.Key.PadRight(20, ' ')}{record.Value}");
+        }
     }
 
     public static void RunWithRecursion()
